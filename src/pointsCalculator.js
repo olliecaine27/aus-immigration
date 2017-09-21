008ffa7741c60e3d5f099a7bbea7164f80da2360
Oliver Caine <olliecaine@gmail.com>
2017-09-22 00:53:28 +1000

    First test

---
+++ src/pointsCalculator.js
@@ -0,0 +1,11 @@
+export default class {
+
+    constructor(config) {
+        this.config = config;
+    }
+
+    pointsOnDate(date) {
+        return 30;
+    }
+
+}
--- test/index.js
+++ test/index.js
@@ -1,8 +1,20 @@
-import { assert } from 'chai';
-import awesomeFunction from '../src/awesomeModule';
+import {assert} from 'chai';
+import PointsCalculator from '../src/pointsCalculator';
 
-describe('Awesome test.', () => {
-  it('should test awesome function', () => {
-    assert(awesomeFunction(1, 1) === 2, 'Not awesome :(');
-  });
-});
+// https://www.border.gov.au/Trav/Visa-1/189-?modal=/visas/supporting/Pages/skilled/the-points-table.aspx
+
+describe('Points on date', () => {
+	describe('Today as a 31 year old', () => {
+		it('Should total 30 points', () => {
+			let config = {
+				dob: new Date(1985, 11, 27)
+			}
+			let pointsCalculator = new PointsCalculator(config);
+
+			let today = new Date(2018, 8, 22);
+			let result = pointsCalculator.pointsOnDate(today);
+
+			assert(result === 30, 'Oops');
+		})
+	})
+})

e73f0211f314fd2824728aee8a800df75779958e
Oliver Caine <olliecaine@gmail.com>
2017-09-22 01:07:21 +1000

    Initial points calc

--- test/index.js
+++ test/index.js
@@ -3,18 +3,74 @@ import PointsCalculator from '../src/pointsCalculator';
 
 // https://www.border.gov.au/Trav/Visa-1/189-?modal=/visas/supporting/Pages/skilled/the-points-table.aspx
 
-describe('Points on date', () => {
-	describe('Today as a 31 year old', () => {
-		it('Should total 30 points', () => {
+describe('pointsOnDate', () => {
+	describe('As an 18 year old', () => {
+		it('Should total 25 points', () => {
 			let config = {
-				dob: new Date(1985, 11, 27)
+				dob: new Date(2000, 0, 1)
 			}
 			let pointsCalculator = new PointsCalculator(config);
 
-			let today = new Date(2018, 8, 22);
+			let today = new Date(2018, 0, 1);
+			let result = pointsCalculator.pointsOnDate(today);
+
+			assert(result === 25, 'Oops');
+		})
+	})
+
+	describe('As a 25 year old', () => {
+		it('Should total 30 points', () => {
+			let config = {
+				dob: new Date(2000, 0, 1)
+			}
+			let pointsCalculator = new PointsCalculator(config);
+
+			let today = new Date(2025, 0, 1);
 			let result = pointsCalculator.pointsOnDate(today);
 
 			assert(result === 30, 'Oops');
+		})
+	})
+
+	describe('As a 33 year old', () => {
+		it('Should total 25 points', () => {
+			let config = {
+				dob: new Date(2000, 0, 1)
+			}
+			let pointsCalculator = new PointsCalculator(config);
+
+			let today = new Date(2033, 0, 1);
+			let result = pointsCalculator.pointsOnDate(today);
+
+			assert(result === 25, 'Oops');
+		})
+	})
+
+	describe('As a 40 year old', () => {
+		it('Should total 15 points', () => {
+			let config = {
+				dob: new Date(2000, 0, 1)
+			}
+			let pointsCalculator = new PointsCalculator(config);
+
+			let today = new Date(2040, 0, 1);
+			let result = pointsCalculator.pointsOnDate(today);
+
+			assert(result === 15, 'Oops');
+		})
+	})
+
+	describe('As a 45 year old', () => {
+		it('Should total 0 points', () => {
+			let config = {
+				dob: new Date(2000, 0, 1)
+			}
+			let pointsCalculator = new PointsCalculator(config);
+
+			let today = new Date(2045, 0, 1);
+			let result = pointsCalculator.pointsOnDate(today);
+
+			assert(result === 0, 'Oops');
 		})
 	})
 })

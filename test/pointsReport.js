939d7f412b3992b3bf1539829411a2c1bfe71120
Oliver Caine <olliecaine@gmail.com>
2017-09-22 01:16:56 +1000

    Points report

---
+++ test/pointsReport.js
@@ -0,0 +1,14 @@
+import PointsCalculator from '../src/pointsCalculator';
+
+it('Points report', () => {
+	let config = {
+		dob: new Date(1985, 11, 27)
+	}
+	let pointsCalculator = new PointsCalculator(config)
+
+	for (var i = 0; i < 30; i++) {
+		let yearCheck = 2018 + i
+		let dateCheck = new Date(yearCheck, 11, 27)
+		console.log(`Ollie\'s total points on year ${yearCheck}:`, pointsCalculator.pointsOnDate(dateCheck))
+	}
+})

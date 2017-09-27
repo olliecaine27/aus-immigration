a019702ec12b3a41be569debe8d6e21a0d9b4c4c
Oliver Caine <olliecaine@gmail.com>
2017-09-27 22:40:41 +1000

    Linting

--- src/pointsCalculator.js
+++ src/pointsCalculator.js
@@ -5,18 +5,18 @@ export default class {
     }
 
     calculateAge(birthday, ageOnDate) {
-        var ageDifMs = ageOnDate - birthday.getTime();
-        var ageDate = new Date(ageDifMs); // miliseconds from epoch
+        const ageDifMs = ageOnDate - birthday.getTime();
+        const ageDate = new Date(ageDifMs); // miliseconds from epoch
         return Math.abs(ageDate.getUTCFullYear() - 1970);
     }
 
     pointsOnDate(date) {
-        let age = this.calculateAge(this.config.dob, date);
-        if(age < 18) return 0;
-        if(age < 25) return 25;
-        if(age < 33) return 30;
-        if(age < 40) return 25;
-        if(age < 45) return 15;
+        const age = this.calculateAge(this.config.dob, date);
+        if (age < 18) return 0;
+        if (age < 25) return 25;
+        if (age < 33) return 30;
+        if (age < 40) return 25;
+        if (age < 45) return 15;
         return 0;
     }
 

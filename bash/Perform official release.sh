5b1e43a2534fdeadab66bafe641da02af6356edc
Oliver Caine <olliecaine@gmail.com>
2017-10-12 13:40:04 +1100

    Committing files so verrsion can be updated

--- bash/Perform official release.sh
+++ bash/Perform official release.sh
@@ -6,11 +6,19 @@ ${0} # run current file
 echo "Enter release type (patch|minor|major): "
 read releaseType
 
+echo "Committing files so verrsion can be updated..."
+git add .
+git commit -m "Committing files so verrsion can be updated"
+
 echo "Updating package.json with new version..."
 npm version $releaseType
 
-echo "Creating git tag..."
 packageVersion=$(node -p "require('./package.json').version")
+
+echo "Committing version change"
+git commit -m "Updating version to $packageVersion"
+
+echo "Creating git tag..."
 git tag "v$packageVersion"
 
 echo "Pushing tags..."
